4abaf9c5ff304773db466f5fb33d5d47e851672e
jerry <woffl_@naver.com>
2022-01-14 11:31:42 +0900

    dsf

--- src/com/java/iu/addressbook/AddressInput.java
+++ src/com/java/iu/addressbook/AddressInput.java
@@ -1,5 +1,6 @@
 package com.java.iu.addressbook;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressInput {
@@ -8,7 +9,23 @@ public class AddressInput {
 	public AddressInput() { //생성자
 		sc = new Scanner(System.in);
 	}
-	public InfoDTO inputInfo( ) {
+	
+	//이름을 입력받아서 같은 이름이 있는 InfoDTO를 찾아서 리턴
+	
+	public InfoDTO searchInfo(List<InfoDTO> ar) {
+		System.out.println("출력을 원하는 이름을 입력하세요.");
+		String name = sc.next();
+		InfoDTO infoDTO = null;
+		for(int i = 0; i<ar.size();i++) {
+			
+			if(ar.get(i).getName().equals(name)) {
+				infoDTO = ar.get(i);
+				break;
+			}
+		}
+		return infoDTO;
+	}
+	public InfoDTO inputInfo() {
 		InfoDTO infoDTO = new InfoDTO();
 		System.out.println("이름을 입력하세요.");
 		infoDTO.setName(sc.next());
